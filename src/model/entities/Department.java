97c1802cfd471c6f351e99904587b01611993d87
n4co <nf.oliveira@hotmail.com>
2021-09-28 00:47:11 -0300

    Adicionando acesso ao Banco de Dados

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,11 +2,10 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable	 {
+public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	
 	private Integer id;
 	private String name;
 	
@@ -38,8 +37,8 @@ public class Department implements Serializable	 {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result *((id == null) ? 0 : id.hashCode());
-	    return result;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -51,36 +50,16 @@ public class Department implements Serializable	 {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-	         if (id == null ) {
-	        	 if(other.id != null) {
-	        		 return false;
-	       }else if (!id.equals(other.id)) 
-	                return false;
-	         }
-	              return true;
-	}         
-	  
-	
-	 @Override
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
-   }	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+}
